9fbbfb9994fecd76bd3cbd28d065eb999c164e6e
Hipork <ShaykhlislamovIn@mail.ru>
2023-02-06 11:54:15 +0300

    v1

--- app.js
+++ app.js
@@ -4,29 +4,27 @@ const turnDagger = document.querySelector('#turnDagger');
 const gameAreaFieldAll = document.querySelectorAll('.game-area__field');
 const gameReset = document.querySelector('.info-area__button_type_reset');
 let isDagger = true;
+let arrayDagger = [];
 
 gameAreaField.addEventListener('click', function (evt) {
   if ((evt.target.classList.contains('game-area__field_krest')) && (evt.target.classList.contains('game-area__field_nolik'))) {
-    console.log('Ничего не нужно делать');   
+ 
   }
 
   if ((!evt.target.classList.contains('game-area__field_krest')) && (!evt.target.classList.contains('game-area__field_nolik'))) {
-    console.log('Да нужно добавлять крестик или нолик');
     if (isDagger) {
-      console.log('Да, я играю за крестики');
-      evt.target.classList.add('game-area__field_krest');   
+      evt.target.classList.add('game-area__field_krest');
+      arrayDagger[evt.target.id - 1] = isDagger;   
       isDagger = !isDagger;
-      checkTurn();
+      console.log(checkVictory(arrayDagger));
     } else {
-      console.log('Нет, я играю за нолики');
       evt.target.classList.add('game-area__field_nolik');
+      arrayDagger[evt.target.id - 1] = isDagger;
       isDagger = !isDagger;
-      checkTurn();
+      console.log(checkVictory(arrayDagger));
     }
   }
 })
-
-console.log(gameAreaFieldAll);
 
 gameReset.addEventListener('click', function () {
   gameAreaFieldAll.forEach(element => {
@@ -34,18 +32,81 @@ gameReset.addEventListener('click', function () {
     element.classList.remove('game-area__field_krest');    
   });
   isDagger = true;
-  turnDagger.classList.add('info-area__turn_off');
-  turnZero.classList.remove('info-area__turn_off');
+  arrayDagger = [];
 })
 
-function checkTurn () {
-  if (isDagger) {
-    turnDagger.classList.toggle('info-area__turn_off');
-    turnZero.classList.toggle('info-area__turn_off');
-  } else {
-    turnDagger.classList.toggle('info-area__turn_off');
-    turnZero.classList.toggle('info-area__turn_off');
+function checkVictory (array) {
+  if ((array[0] === true) && (array[1] === true) && (array[2] === true)) {
+    return 'Victory Dugger';    
+  }   
+
+  if ((array[0] === false) && (array[1] === false) && (array[2]) === false) {
+    return 'Victory Zero';
   }
+
+  if ((array[3] === true) && (array[4] === true) && (array[5] === true)) {
+    return 'Victory Dugger';  
+  }  
+
+  if ((array[3] === false) && (array[4] === false) && (array[5]) === false) {
+    return 'Victory Zero';
+  }
+
+  if ((array[6] === true) && (array[7] === true) && (array[8] === true)) {
+    return 'Victory Dugger'; 
+  } 
+
+  if ((array[6] === false) && (array[7] === false) && (array[8]) === false) {
+    return 'Victory Zero';
+  }
+
+  if ((array[0] === true) && (array[3] === true) && (array[6] === true)) {
+    return 'Victory Dugger';     
+  } 
+
+  if ((array[0] === false) && (array[3] === false) && (array[6]) === false) {
+    return 'Victory Zero';
+  }
+
+  if ((array[1] === true) && (array[4] === true) && (array[7] === true)) {
+    return 'Victory Dugger';     
+  }
+
+  if ((array[1] === false) && (array[4] === false) && (array[7]) === false) {
+    return 'Victory Zero';
+  }
+  
+  if ((array[2] === true) && (array[5] === true) && (array[8] === true)) {
+    return 'Victory Dugger';     
+  } 
+
+  if ((array[2] === false) && (array[5] === false) && (array[8]) === false) {
+    return 'Victory Zero';
+  }
+
+  if ((array[0] === true) && (array[4] === true) && (array[8] === true)) {
+    return 'Victory Dugger';     
+  } 
+
+  if ((array[0] === false) && (array[4] === false) && (array[8]) === false) {
+    return 'Victory Zero';
+  }
+
+  if ((array[2] === true) && (array[4] === true) && (array[6] === true)) {
+    return 'Victory Dugger';     
+  } 
+
+  if ((array[2] === false) && (array[4] === false) && (array[6]) === false) {
+    return 'Victory Zero';
+  }
+
+  if ((!array.includes(undefined)) && array.length === 9) {
+    return 'DRAW'
+  }
+
 }
+
+
+
 
 
